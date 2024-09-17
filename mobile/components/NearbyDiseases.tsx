e46ac1b31988c7d04e6d8a6950c82e59ebfb0ba8
jabedzaman <hi@jabed.dev>
2024-09-17 17:53:31 +0530

    feat: list diseases

--- mobile/components/NearbyDiseases.tsx
+++ mobile/components/NearbyDiseases.tsx
@@ -1,22 +1,40 @@
-import { useData } from '@/contexts/DataContext'
-import React from 'react'
-import { View } from 'react-native'
-import { ThemedText } from './ThemedText'
+import { useData } from "@/contexts/DataContext";
+import { DiseaseReponseType } from "@/types";
+import { FlashList } from "@shopify/flash-list";
+import React from "react";
+import { View } from "react-native";
+import { ThemedText } from "./ThemedText";
 
 export default function NearbyDiseases() {
-    const { nearbyDiseases } = useData()
-    console.log(nearbyDiseases[0])
-    return (
-        <View>
-            {
-                nearbyDiseases.map((disease, index) => {
-                    return (
-                        <View key={index}>
-                            <ThemedText>{disease.disease}</ThemedText>
-                        </View>
-                    )
-                })
-            }
-        </View>
-    )
+  const { nearbyDiseases } = useData();
+  console.log(nearbyDiseases);
+  return (
+    <FlashList
+      renderItem={({ item }) => {
+        return <Item {...item} />;
+      }}
+      contentContainerStyle={{ paddingHorizontal: 10 }}
+      estimatedItemSize={100}
+      keyExtractor={(item) => item.id}
+      data={nearbyDiseases}
+    />
+  );
 }
+
+const Item: React.FC<DiseaseReponseType> = (disease) => {
+  return (
+    <View
+      style={{
+        borderWidth: 0.5,
+        borderColor: "#cecece",
+        borderRadius: 10,
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+        marginVertical: 5,
+      }}
+    >
+      <ThemedText>{disease.disease}</ThemedText>
+      <ThemedText>{disease.cause}</ThemedText>
+    </View>
+  );
+};
