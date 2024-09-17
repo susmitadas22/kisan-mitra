f47502082bd0e82489515e193ce0e83708d74693
jabedzaman <hi@jabed.dev>
2024-09-17 07:56:50 +0530

    ƒeat: use logto for auth

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,43 +1,72 @@
+import { useColorScheme } from "@/hooks/useColorScheme";
 import {
-    DarkTheme,
-    DefaultTheme,
-    ThemeProvider,
-} from '@react-navigation/native'
-import { useFonts } from 'expo-font'
-import { Stack } from 'expo-router'
-import * as SplashScreen from 'expo-splash-screen'
-import { useEffect } from 'react'
-import 'react-native-reanimated'
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Slot } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/useColorScheme'
+import { LogtoConfig, LogtoProvider } from "@logto/rn";
+
+const config: LogtoConfig = {
+  endpoint: "https://auth.jabed.dev/",
+  appId: "q9dargddx63xp64dxwhfi",
+};
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync()
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-    const colorScheme = useColorScheme()
-    const [loaded] = useFonts({
-        SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    })
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
 
-    useEffect(() => {
-        if (loaded) {
-            SplashScreen.hideAsync()
-        }
-    }, [loaded])
-
-    if (!loaded) {
-        return null
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
     }
+  }, [loaded]);
 
-    return (
-        <ThemeProvider
-            value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}
-        >
-            <Stack>
-                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-                <Stack.Screen name="+not-found" />
-            </Stack>
-        </ThemeProvider>
-    )
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <LogtoProvider config={config}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Slot />
+      </ThemeProvider>
+    </LogtoProvider>
+  );
 }
+
+// function Layout() {
+//   const { isAuthenticated, isInitialized } = useLogto();
+//   console.log({ isAuthenticated, isInitialized });
+//   if (isAuthenticated && isInitialized) {
+//     return <AppLayout />;
+//   }
+//   return <AuthLayout />;
+// }
+
+// function AuthLayout() {
+//   return (
+//     <Stack>
+//       <Stack.Screen name="sign-in" />
+//     </Stack>
+//   );
+// }
+
+// function AppLayout() {
+//   return (
+//     <Stack>
+//       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+//       <Stack.Screen name="+not-found" />
+//     </Stack>
+//   );
+// }
