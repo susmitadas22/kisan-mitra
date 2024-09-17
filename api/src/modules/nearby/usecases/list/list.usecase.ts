9d6d1a7119f38a84eda5efa891f0c0a86cdf3e0b
jabedzaman <hi@jabed.dev>
2024-09-17 18:48:48 +0530

    feat: dont send null values

--- api/src/modules/nearby/usecases/list/list.usecase.ts
+++ api/src/modules/nearby/usecases/list/list.usecase.ts
@@ -7,13 +7,31 @@ export class List {
   private logger = new Logger(List.name);
   constructor(private readonly prismaService: PrismaService) {}
   async execute(command: ListCommand) {
-    const { lat, lng } = command;
+    const { lat, lng, sub } = command;
     this.logger.log(`Listing nearby users at ${lat}, ${lng}`);
     /**
      * all entries within 10km of the given lat/lng
      */
     const nearby = await this.prismaService.image.findMany({
       where: {
+        cause: {
+          not: null,
+        },
+        disease: {
+          not: null,
+        },
+        preventions: {
+          not: null,
+        },
+        sub: {
+          not: sub,
+        },
+        cure: {
+          not: null,
+        },
+        symptoms: {
+          not: null,
+        },
         lat: {
           gte: lat - 0.1, // 0.1 is 10km
           lte: lat + 0.1, // 0.1 is 10km
