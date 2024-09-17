9008cbc71c3896237bfc4391e7256aa478a63b2a
sushmitadas22 <susmitadas9639@gmail.com>
2024-09-17 10:31:17 +0530

    feat: use transformers to get idea

--- apps/ml/main.py
+++ apps/ml/main.py
@@ -1,5 +1,11 @@
-# Use a pipeline as a high-level helper
 from transformers import pipeline
+
 
 pipe = pipeline("image-classification", model="gianlab/swin-tiny-patch4-window7-224-finetuned-plantdisease")
 
+# image_path = "./sample/tomato_mosaic_virus.png"
+image_path = "./sample/potato_late_blight.png"
+
+result = pipe(image_path)
+
+print(result)
