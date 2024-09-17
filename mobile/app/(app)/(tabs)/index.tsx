436059b9a1c4020dcdf7fc57efa226fff5b32b58
jabedzaman <hi@jabed.dev>
2024-09-17 14:31:56 +0530

    ci: sync with remote

--- mobile/app/(app)/(tabs)/index.tsx
+++ mobile/app/(app)/(tabs)/index.tsx
@@ -1,81 +1,79 @@
-import LocationComponent from '@/components/Location';
-import { globalStyles } from '@/constants/styles';
-import useImageUpload from '@/hooks/useImageUpload';
-import { Ionicons } from '@expo/vector-icons';
-import { useLogto } from '@logto/rn';
-import Axios from 'axios';
-import * as ImagePicker from 'expo-image-picker';
-import React, { useEffect, useState } from 'react';
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import LocationComponent from "@/components/Location";
+import { globalStyles } from "@/constants/styles";
+import useImageUpload from "@/hooks/useImageUpload";
+import { Ionicons } from "@expo/vector-icons";
+import { useLogto } from "@logto/rn";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import DocumentPicker, {
+  DocumentPickerResponse,
+} from "react-native-document-picker";
+
 export default function Home() {
-  const [image, setImage] = useState<string | null>(null);
+  const [image, setImage] = useState<DocumentPickerResponse | null>(null);
+  const [sub, setSub] = useState<string | null>(null);
   const { uploadImage } = useImageUpload();
   const { getIdTokenClaims } = useLogto();
-  const [id, setID] = useState("");
+
+  const pickImage = async () => {
+    try {
+      const res = await DocumentPicker.pick({
+        type: [DocumentPicker.types.images],
+        mode: "import",
+        allowMultiSelection: false,
+        copyTo: "cachesDirectory",
+      });
+      setImage(res[0]);
+    } catch (err) {}
+  };
+
+  const handleUpload = async () => {
+    if (!sub) return;
+    const formData = new FormData();
+    formData.append("sub", sub);
+    if (image) {
+      formData.append("image", {
+        uri: image.uri,
+        type: image.type,
+        name: image.name,
+      });
+    }
+    try {
+      const { data } = await axios.put(
+        "http://192.168.232.76:3000/api/v1/uploads",
+        formData
+      );
+      console.log(data);
+    } catch (error: any) {
+      console.log(error.response.data);
+    }
+  };
+
   const setUser = async () => {
     const { sub } = await getIdTokenClaims();
-    setID(sub);
-    console.log(sub);
+    setSub(sub);
   };
   useEffect(() => {
     setUser();
   }, []);
-
-  const pickImage = async () => {
-    // No permissions request is necessary for launching the image library
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
-
-    console.log(result);
-
-    if (!result.canceled) {
-      setImage(result.assets[0].uri);
-    }
-  };
-
-  const handleUpload = async () => {
-    const formData = new FormData();
-    formData.append('image', new File([image], 'image.jpg', { type: "image/png" || "image/jpg" || "image/jpeg" }));
-    formData.append('sub', id);
-    Axios.put('http://192.168.232.76:3000/api/v1/uploads', formData)
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
   return (
     <View style={globalStyles.pageWrapper}>
       <LocationComponent />
-      {
-        !image &&
-        <TouchableOpacity
-          onPress={pickImage}
-          style={globalStyles.button}
-        >
-          <Ionicons name='camera' size={20} />
-          <Text style={globalStyles.buttonText}>
-            Select Crop Image
-          </Text>
+      {!image && (
+        <TouchableOpacity onPress={pickImage} style={globalStyles.button}>
+          <Ionicons name="camera" size={20} />
+          <Text style={globalStyles.buttonText}>Select Crop Image</Text>
         </TouchableOpacity>
-      }
-      {
-        image &&
+      )}
+      {image && (
         <View style={globalStyles.horizontal}>
-          <Image source={{ uri: image }} style={styles.image} />
-
-          <TouchableOpacity
-            style={globalStyles.button}
-          >
+          <Image source={{ uri: image.uri }} style={styles.image} />
+          <TouchableOpacity style={globalStyles.button} onPress={handleUpload}>
             <Text style={globalStyles.buttonText}>Upload</Text>
           </TouchableOpacity>
         </View>
-      }
+      )}
     </View>
   );
 }
@@ -88,7 +86,7 @@ const styles = StyleSheet.create({
   },
   image: {
     width: "100%",
-    objectFit: 'cover',
+    objectFit: "cover",
     height: 200,
     borderRadius: 5,
   },
